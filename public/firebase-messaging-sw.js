f79c75518dd579e5f82b1f32228e6b3003b38737
Sandro Alencar Fernandes <sandro@arealocal.com.br>
2025-08-22 10:51:14 -0300

    ajustes para funcionar o firebase-message

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+// public/firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging-compat.js');
+const firebaseConfig = {
+    apiKey: "AIzaSyDEQ0L3QYUKxXucJjMe30lrI0_XZmcDNZk",
+    authDomain: "services-sandroaf.firebaseapp.com",
+    projectId: "services-sandroaf",
+    storageBucket: "services-sandroaf.appspot.com",
+    messagingSenderId: "8530075618",
+    appId: "1:8530075618:web:476219e15e278d98d2a3bb",
+    measurementId: "G-STZ7LQHZK3"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
